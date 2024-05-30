54b1b0c54c34f089c3ea4c5a5987b709759e50b5
Timothy Osmond <45649121+timothyosm@users.noreply.github.com>
2024-05-30 23:43:24 +1000

    tailwind + shadcn + ts added

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,19 +1,22 @@
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 
 const routes = [
     {
-        path: '/',
+        path: "/",
         element: <div> Hello Laravel 11 from React 18</div>,
     },
     {
-        path: '/boop',
-        element: <div> Boop front end route</div>,
-    }
+        path: "/boop",
+        element: (
+            <div>
+                <Button>Click me</Button>
+            </div>
+        ),
+    },
 ];
 
-createRoot(document.getElementById('root')).render(
-    <RouterProvider
-        router={createBrowserRouter(routes)}
-    />
-)
+createRoot(document.getElementById("root")).render(
+    <RouterProvider router={createBrowserRouter(routes)} />
+);
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
-import 'dotenv/config';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import "dotenv/config";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: ["resources/css/app.css", "resources/js/app.jsx"],
             refresh: true,
         }),
         react(),
