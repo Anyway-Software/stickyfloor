a28de98749e945c0d20618abc98c7b1648b65d7f
James McGlone <james@anyway.software>
2024-05-30 17:51:37 +0800

    Routes for auth

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+
+    Route::group(['middleware' => 'auth:sanctum'], function() {
+      Route::get('logout', [AuthController::class, 'logout']);
+      Route::get('user', [AuthController::class, 'user']);
+    });
+});
 
 Route::get('/health', function (Request $request) {
     return response()->json(['status' => 'API healthy ok! gfo!']);
