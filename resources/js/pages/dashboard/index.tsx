12b5df958690150ae98f915c8444a5bdcd71832a
James McGlone <james@anyway.software>
2024-06-14 02:29:41 +0800

    Events list table on dashboard

--- resources/js/pages/dashboard/index.tsx
+++ resources/js/pages/dashboard/index.tsx
@@ -298,16 +298,11 @@ export function Dashboard() {
                     </Card>
                 </div>
                 <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
-                    <Card
-                        className="xl:col-span-2"
-                        x-chunk="dashboard-01-chunk-4"
-                    >
+                    <Card className="xl:col-span-2" x-chunk="dashboard-01-chunk-4">
                         <CardHeader className="flex flex-row items-center">
                             <div className="grid gap-2">
-                                <CardTitle>Transactions</CardTitle>
-                                <CardDescription>
-                                    Recent transactions from your store.
-                                </CardDescription>
+                                <CardTitle>Events</CardTitle>
+                                <CardDescription>Your events.</CardDescription>
                             </div>
                             <Button asChild size="sm" className="ml-auto gap-1">
                                 <Link href="#">
@@ -320,161 +315,34 @@ export function Dashboard() {
                             <Table>
                                 <TableHeader>
                                     <TableRow>
-                                        <TableHead>Customer</TableHead>
-                                        <TableHead className="hidden xl:table-column">
-                                            Type
-                                        </TableHead>
-                                        <TableHead className="hidden xl:table-column">
-                                            Status
-                                        </TableHead>
-                                        <TableHead className="hidden xl:table-column">
-                                            Date
-                                        </TableHead>
-                                        <TableHead className="text-right">
-                                            Amount
-                                        </TableHead>
+                                        <TableHead>Event Name</TableHead>
+                                        <TableHead className="">Venue Name</TableHead>
+                                        <TableHead className="">Description</TableHead>
+                                        {/* <TableHead className="hidden xl:table-column">Created At</TableHead> */}
                                     </TableRow>
                                 </TableHeader>
                                 <TableBody>
-                                    <TableRow>
-                                        <TableCell>
-                                            <div className="font-medium">
-                                                Liam Johnson
-                                            </div>
-                                            <div className="hidden text-sm text-muted-foreground md:inline">
-                                                liam@example.com
-                                            </div>
-                                        </TableCell>
-                                        <TableCell className="hidden xl:table-column">
-                                            Sale
-                                        </TableCell>
-                                        <TableCell className="hidden xl:table-column">
-                                            <Badge
-                                                className="text-xs"
-                                                variant="outline"
-                                            >
-                                                Approved
-                                            </Badge>
-                                        </TableCell>
-                                        <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                                            2023-06-23
-                                        </TableCell>
-                                        <TableCell className="text-right">
-                                            $250.00
-                                        </TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>
-                                            <div className="font-medium">
-                                                Olivia Smith
-                                            </div>
-                                            <div className="hidden text-sm text-muted-foreground md:inline">
-                                                olivia@example.com
-                                            </div>
-                                        </TableCell>
-                                        <TableCell className="hidden xl:table-column">
-                                            Refund
-                                        </TableCell>
-                                        <TableCell className="hidden xl:table-column">
-                                            <Badge
-                                                className="text-xs"
-                                                variant="outline"
-                                            >
-                                                Declined
-                                            </Badge>
-                                        </TableCell>
-                                        <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                                            2023-06-24
-                                        </TableCell>
-                                        <TableCell className="text-right">
-                                            $150.00
-                                        </TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>
-                                            <div className="font-medium">
-                                                Noah Williams
-                                            </div>
-                                            <div className="hidden text-sm text-muted-foreground md:inline">
-                                                noah@example.com
-                                            </div>
-                                        </TableCell>
-                                        <TableCell className="hidden xl:table-column">
-                                            Subscription
-                                        </TableCell>
-                                        <TableCell className="hidden xl:table-column">
-                                            <Badge
-                                                className="text-xs"
-                                                variant="outline"
-                                            >
-                                                Approved
-                                            </Badge>
-                                        </TableCell>
-                                        <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                                            2023-06-25
-                                        </TableCell>
-                                        <TableCell className="text-right">
-                                            $350.00
-                                        </TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>
-                                            <div className="font-medium">
-                                                Emma Brown
-                                            </div>
-                                            <div className="hidden text-sm text-muted-foreground md:inline">
-                                                emma@example.com
-                                            </div>
-                                        </TableCell>
-                                        <TableCell className="hidden xl:table-column">
-                                            Sale
-                                        </TableCell>
-                                        <TableCell className="hidden xl:table-column">
-                                            <Badge
-                                                className="text-xs"
-                                                variant="outline"
-                                            >
-                                                Approved
-                                            </Badge>
-                                        </TableCell>
-                                        <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                                            2023-06-26
-                                        </TableCell>
-                                        <TableCell className="text-right">
-                                            $450.00
-                                        </TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>
-                                            <div className="font-medium">
-                                                Liam Johnson
-                                            </div>
-                                            <div className="hidden text-sm text-muted-foreground md:inline">
-                                                liam@example.com
-                                            </div>
-                                        </TableCell>
-                                        <TableCell className="hidden xl:table-column">
-                                            Sale
-                                        </TableCell>
-                                        <TableCell className="hidden xl:table-column">
-                                            <Badge
-                                                className="text-xs"
-                                                variant="outline"
-                                            >
-                                                Approved
-                                            </Badge>
-                                        </TableCell>
-                                        <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                                            2023-06-27
-                                        </TableCell>
-                                        <TableCell className="text-right">
-                                            $550.00
-                                        </TableCell>
-                                    </TableRow>
+                                    {events.map(event => (
+                                        <TableRow key={event.id}>
+                                            <TableCell>
+                                                <div className="font-medium">{event.event_name}</div>
+                                            </TableCell>
+                                            <TableCell className="">
+                                                {event.venue_name}
+                                            </TableCell>
+                                            <TableCell className="">
+                                                {event.event_description}
+                                            </TableCell>
+                                            {/* <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                                                {new Date(event.created_at).toLocaleDateString()}
+                                            </TableCell> */}
+                                        </TableRow>
+                                    ))}
                                 </TableBody>
                             </Table>
                         </CardContent>
                     </Card>
+
                     <Card x-chunk="dashboard-01-chunk-5">
                         <CardHeader>
                             <CardTitle>Recent Sales</CardTitle>
