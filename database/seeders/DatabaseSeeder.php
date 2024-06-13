f7a5f538172c42a86e17192078be6b57421970cb
James McGlone <james@anyway.software>
2024-06-14 05:41:31 +0800

    Update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Event;
+use App\Models\TicketCategory;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -31,10 +32,47 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('Password99'),
         ]);
 
-        Event::factory(4)->create([
-            'user_id' => $user->id,
-        ]);
+        // Event::factory(4)->create([
+        //     'user_id' => $user->id,
+        // ])->each(function ($event) {
+        //     TicketCategory::factory(rand(1, 3))->create([
+        //         'event_id' => $event->id,
+        //     ]);
+        // });
 
+        Event::factory()->create([
+            'event_name' => 'Blazing Swan',
+            'venue_name' => 'Kulin Racecourse',
+            'user_id' => $user->id,
+        ])->each(function ($event) {
+            TicketCategory::factory()->create([
+                'event_id' => $event->id,
+                'name' => 'Theme Camp',
+                'price' => 200,
+            ]);
+            TicketCategory::factory()->create([
+                'event_id' => $event->id,
+                'name' => '1st round general admission',
+                'price' => 250,
+            ]);
+            TicketCategory::factory()->create([
+                'event_id' => $event->id,
+                'name' => '2nd round general admission',
+                'price' => 300,
+            ]);
+        });
+
+        Event::factory()->create([
+            'event_name' => 'Toasty Beats',
+            'venue_name' => 'Vinyl Cafe',
+            'user_id' => $user->id,
+        ])->each(function ($event) {
+            TicketCategory::factory()->create([
+                'event_id' => $event->id,
+                'name' => 'General Admission',
+                'price' => 10,
+            ]);
+        });
         // $user->events()->createMany([
         //     [
         //         'id' => Str::uuid(),
