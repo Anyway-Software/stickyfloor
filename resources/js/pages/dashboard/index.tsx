adf8c91bd5141038cb1861eaccd1af0bdadbb9e8
James McGlone <james@anyway.software>
2024-06-14 22:28:50 +0800

    tiny change to test deploy

--- resources/js/pages/dashboard/index.tsx
+++ resources/js/pages/dashboard/index.tsx
@@ -144,7 +144,7 @@ export function Dashboard() {
                         href="#"
                         className="text-muted-foreground transition-colors hover:text-foreground"
                     >
-                        Analytica
+                        Analytics
                     </Link>
                 </nav>
                 <Sheet>
@@ -194,7 +194,7 @@ export function Dashboard() {
                                 href="#"
                                 className="text-muted-foreground hover:text-foreground"
                             >
-                                Analytica
+                                Analytics
                             </Link>
                         </nav>
                     </SheetContent>
