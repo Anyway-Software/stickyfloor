efd0b4c1c824c44df561e8884093d9cf628d1798
James McGlone <james@anyway.software>
2024-10-15 23:08:55 +0800

    Test runner

--- resources/js/pages/landing/index.tsx
+++ resources/js/pages/landing/index.tsx
@@ -2,6 +2,7 @@ export function Landing() {
     return (
         <div className="flex items-center justify-center h-screen">
             <div className="text-4xl font-bold">Welcome to stickyfloor.</div>
+            <div className="text-4xl font-bold">Tickets without the ridiculous booking fees</div>
         </div>
     );
 }
