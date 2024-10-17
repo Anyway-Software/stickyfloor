6f3f5d300f522f5833ddeec9d8287437c60e0c57
James McGlone <james@anyway.software>
2024-10-17 14:56:43 +0800

    Rename create event route to use hyphen not underline

--- resources/js/routes/create_event.lazy.tsx
+++ resources/js/routes/create_event.lazy.tsx
@@ -2,7 +2,7 @@ import { NavShell } from "@/components/shared/NavBar";
 import { CreateEvent } from "@/pages/CreateEvents";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
-export const Route = createLazyFileRoute("/create_event")({
+export const Route = createLazyFileRoute("/create-event")({
     component: () => (
         <NavShell>
             <CreateEvent />
