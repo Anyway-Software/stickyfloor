75718db1cb4247d317143dd3998ecabac621edeb
James McGlone <james@anyway.software>
2024-10-22 14:53:24 +0800

    Nav bar is expandable

--- resources/js/components/shared/NavBar.tsx
+++ resources/js/components/shared/NavBar.tsx
@@ -1,25 +1,14 @@
 import { Link, useLocation, useNavigate } from '@tanstack/react-router'
 import {
-    Bird,
-    Book,
-    Bot,
-    Code2,
-    FootprintsIcon,
-    LifeBuoy,
-    Rabbit,
-    Settings,
-    Settings2,
-    Share,
     SquareTerminal,
-    SquareUser,
-    Ticket,
     Calendar,
-    Turtle,
     PlusCircle,
+    Ticket,
+    SquareUser,
 } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
 import { Button } from '@/components/ui/button'
-import { Textarea } from '@/components/ui/textarea'
 import {
     Tooltip,
     TooltipContent,
@@ -40,14 +29,35 @@ const navItems = [
     { label: 'Events', icon: Calendar, route: '/events' },
     { label: 'Create Event', icon: PlusCircle, route: '/create_event' },
     { label: 'Tickets', icon: Ticket, route: '/tickets' },
-    // { label: 'Customers', icon: SquareUser },
-    // { label: 'Reports', icon: Book },
-    // { label: 'Settings', icon: Settings2 },
 ]
 
 export function NavShell({ children }: { children: any }) {
     const location = useLocation()
     const navigate = useNavigate()
+
+    const [isExpanded, setIsExpanded] = useState(() => {
+        // Initialize from localStorage, default to true if not set
+        const storedState = localStorage.getItem('sidebar-expanded')
+        return storedState === null ? true : JSON.parse(storedState)
+    })
+
+    const [isTransitionComplete, setIsTransitionComplete] = useState(isExpanded)
+
+    // Update local storage whenever the state changes
+    useEffect(() => {
+        localStorage.setItem('sidebar-expanded', JSON.stringify(isExpanded))
+
+        // Delay setting the transition complete state to match the expansion duration
+        const timeout = setTimeout(() => {
+            setIsTransitionComplete(isExpanded)
+        }, 300) // Match this duration with the CSS transition duration
+
+        return () => clearTimeout(timeout)
+    }, [isExpanded])
+
+    const handleToggleSidebar = () => {
+        setIsExpanded(!isExpanded)
+    }
 
     const handleLogout = () => {
         localStorage.removeItem('api_token')
@@ -55,86 +65,174 @@ export function NavShell({ children }: { children: any }) {
     }
 
     return (
-        <div className="grid h-screen w-full pl-[53px]">
-            <aside className="inset-y fixed left-0 z-20 flex h-full flex-col border-r">
-                <div className="border-b p-2">
-                    <Link to="/dashboard">
-                        <Button variant="outline" size="icon" aria-label="Home">
-                            <img src="/logo.webp" />
-                        </Button>
-                    </Link>
-                </div>
-                <TooltipProvider>
-                    <nav className="grid gap-1 p-2">
-                        {navItems.map((item) => (
-                            <Tooltip key={item.label}>
+        <div className="flex h-screen w-full">
+            <aside
+                className={`flex-shrink-0 transition-all duration-300 ${
+                    isExpanded ? 'w-64' : 'w-14'
+                }`}
+            >
+                <div className="flex h-full flex-col border-r transition-all duration-300 relative">
+                    <div className="border-b p-2 flex items-center justify-between relative">
+                        <Link
+                            to="/dashboard"
+                            className="flex items-center gap-2"
+                        >
+                            <Button
+                                variant="ghost"
+                                size="icon"
+                                aria-label="Home"
+                            >
+                                <img src="/logo.webp" />
+                            </Button>
+                            {isExpanded && (
+                                <span
+                                    className={`font-bold whitespace-nowrap transition-opacity duration-300 ${
+                                        isTransitionComplete
+                                            ? 'opacity-100'
+                                            : 'opacity-0'
+                                    }`}
+                                >
+                                    stickyfloor.
+                                </span>
+                            )}
+                        </Link>
+                        <TooltipProvider>
+                            <Tooltip>
                                 <TooltipTrigger asChild>
-                                    <Link
-                                        to={item.route}
-                                        className={
-                                            location.pathname === item.route
-                                                ? 'bg-green'
-                                                : ''
-                                        }
+                                    <Button
+                                        variant="ghost"
+                                        size="icon"
+                                        onClick={handleToggleSidebar}
+                                        aria-label="Toggle"
+                                        className={`absolute transition-all ${
+                                            isExpanded ? 'right-2' : '-right-12'
+                                        }`}
+                                        style={{
+                                            top: '50%',
+                                            transform: 'translateY(-50%)',
+                                            zIndex: 10,
+                                        }}
                                     >
-                                        <Button
-                                            variant="ghost"
-                                            size="icon"
-                                            className="rounded-lg"
-                                            aria-label={item.label}
-                                        >
-                                            <item.icon className="size-5" />
-                                        </Button>
-                                    </Link>
+                                        {isExpanded ? '<' : '>'}
+                                    </Button>
                                 </TooltipTrigger>
                                 <TooltipContent side="right" sideOffset={5}>
-                                    {item.label}
+                                    {isExpanded ? 'Shrink' : 'Expand'}
                                 </TooltipContent>
                             </Tooltip>
-                        ))}
-                    </nav>
-                    <nav className="mt-auto grid gap-1 p-2">
-                        <Tooltip key={'Account'}>
-                            <TooltipTrigger asChild>
-                                <DropdownMenu>
-                                    <DropdownMenuTrigger asChild>
-                                        <Button
-                                            variant="ghost"
-                                            size="icon"
-                                            className="mt-auto rounded-lg"
-                                            aria-label={'Account'}
+                        </TooltipProvider>
+                    </div>
+                    <TooltipProvider>
+                        <nav className="grid gap-1 p-2">
+                            {navItems.map((item) => (
+                                <Tooltip key={item.label} disabled={isExpanded}>
+                                    <TooltipTrigger asChild>
+                                        <Link
+                                            to={item.route}
+                                            className={`flex items-center gap-2 p-2 rounded-md transition-colors ${
+                                                location.pathname === item.route
+                                                    ? 'bg-green'
+                                                    : ''
+                                            }`}
+                                            style={{
+                                                height: '48px', // Fixed height for consistency
+                                                overflow: 'hidden',
+                                                whiteSpace: 'nowrap',
+                                                textOverflow: 'ellipsis',
+                                            }}
                                         >
-                                            <SquareUser className="size-5" />
-                                        </Button>
-                                    </DropdownMenuTrigger>
-                                    <DropdownMenuContent align="end">
-                                        <DropdownMenuLabel>
-                                            My Account
-                                        </DropdownMenuLabel>
-                                        <DropdownMenuSeparator />
-                                        <DropdownMenuItem>
-                                            Settings
-                                        </DropdownMenuItem>
-                                        <DropdownMenuItem>
-                                            Support
-                                        </DropdownMenuItem>
-                                        <DropdownMenuSeparator />
-                                        <DropdownMenuItem
-                                            onSelect={handleLogout}
+                                            <item.icon className="size-5" />
+                                            {isExpanded && (
+                                                <span
+                                                    className={`ml-2 transition-opacity duration-300 ${
+                                                        isTransitionComplete
+                                                            ? 'opacity-100'
+                                                            : 'opacity-0'
+                                                    }`}
+                                                >
+                                                    {item.label}
+                                                </span>
+                                            )}
+                                        </Link>
+                                    </TooltipTrigger>
+                                    {!isExpanded && (
+                                        <TooltipContent
+                                            side="right"
+                                            sideOffset={5}
                                         >
-                                            Logout
-                                        </DropdownMenuItem>
-                                    </DropdownMenuContent>
-                                </DropdownMenu>
-                            </TooltipTrigger>
-                            <TooltipContent side="right" sideOffset={5}>
-                                Account
-                            </TooltipContent>
-                        </Tooltip>
-                    </nav>
-                </TooltipProvider>
+                                            {item.label}
+                                        </TooltipContent>
+                                    )}
+                                </Tooltip>
+                            ))}
+                        </nav>
+                        <nav className="mt-auto grid gap-1 p-2">
+                            <Tooltip key={'Account'} disabled={isExpanded}>
+                                <TooltipTrigger asChild>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Link
+                                                to="#"
+                                                className={`flex items-center gap-2 p-2 rounded-md transition-colors ${
+                                                    location.pathname ===
+                                                    '/account'
+                                                        ? 'bg-green'
+                                                        : ''
+                                                }`}
+                                                style={{
+                                                    height: '48px', // Fixed height for consistency
+                                                    overflow: 'hidden',
+                                                    whiteSpace: 'nowrap',
+                                                    textOverflow: 'ellipsis',
+                                                }}
+                                            >
+                                                <SquareUser className="size-5" />
+                                                {isExpanded && (
+                                                    <span
+                                                        className={`ml-2 transition-opacity duration-300 ${
+                                                            isTransitionComplete
+                                                                ? 'opacity-100'
+                                                                : 'opacity-0'
+                                                        }`}
+                                                    >
+                                                        Account
+                                                    </span>
+                                                )}
+                                            </Link>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="end">
+                                            <DropdownMenuLabel>
+                                                Account
+                                            </DropdownMenuLabel>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem>
+                                                Settings
+                                            </DropdownMenuItem>
+                                            <DropdownMenuItem>
+                                                Support
+                                            </DropdownMenuItem>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem
+                                                onSelect={handleLogout}
+                                            >
+                                                Logout
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TooltipTrigger>
+                                {!isExpanded && (
+                                    <TooltipContent side="right" sideOffset={5}>
+                                        Account
+                                    </TooltipContent>
+                                )}
+                            </Tooltip>
+                        </nav>
+                    </TooltipProvider>
+                </div>
             </aside>
-            <div className="flex flex-col pt-10 bg-muted">{children}</div>
+            <div className="flex-grow transition-all duration-300">
+                <div className="flex flex-col pt-10 bg-muted">{children}</div>
+            </div>
         </div>
     )
 }
